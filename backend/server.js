c45310555cde9858470c78b95268e85245710fa5
Anindita531 <aninditaghosh3551@gmail.com>
2026-07-19 17:51:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://smartbussystem-1.onrender.com/',
     credentials: true
   }
 })
